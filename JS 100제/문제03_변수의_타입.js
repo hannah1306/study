fb864b6c76cff8f5721bdf3953f34a3d3df2e43d
hannah1306 <49933457+hannah1306@users.noreply.github.com>
2020-06-03 14:59:54 +0900

    Create 문제03_변수의_타입.js

---
+++ JS 100제/문제03_변수의_타입.js
@@ -0,0 +1,20 @@
+/**
+ * 문제03 : 변수의 타입
+ * 다음 출력 값으로 올바른 것은?
+ * 
+ * var arr = [100, 200, 300];
+ * console.log(typeof(arr));
+ * 
+ * 1)  undefined
+ * 2)  string
+ * 3)  number
+ * 4)  object
+ */
+
+var arr = [100, 200, 300];
+console.log(typeof(arr));  //object
+
+//* check
+console.log(typeof undefined)     //'undefined'
+console.log(typeof null)          //'object'
+console.log(typeof function(){})  //'function'
