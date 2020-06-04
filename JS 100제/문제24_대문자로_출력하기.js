4365a78618aefc273172562be44f65d91e99f2ec
hannah1306 <49933457+hannah1306@users.noreply.github.com>
2020-06-04 16:02:48 +0900

    Create 문제24_대문자로_출력하기.js

---
+++ JS 100제/문제24_대문자로_출력하기.js
@@ -0,0 +1,16 @@
+/**
+ * 문제 24 : 대문자로 출력하기
+ * 이름이 입력되면 전부 대문자로 출력되는 프로그램 만들기
+ * 
+ * 입출력
+ * 입력 : mary
+ * 출력 : MARY
+ */
+
+let name = prompt('이름을 입력해주세요');
+console.log(name.toUpperCase());  //MARY
+
+/**
+ * check
+ * 소문자로 변환하려면 toLowerCase()를 사용
+ */
